ce5eb9268fa83f863c4e6879dade2b34126d7d70
Nishan Acharya <n1sh4n12@gmail.com>
2026-09-18 23:37:10 +0545

    Update factorial.kt

--- 2026-09-18/factorial.kt
+++ 2026-09-18/factorial.kt
@@ -1,11 +1,11 @@
-fun main(){
-    val n = listOf(1,3,2,5,2,1,5,6,7)
-    var n1: =listOf(n[1])
-    for (i in 1..n.count()){
-        for(j in 1..n1.count()){
-            if(n1[j]!=n[i]){
-                n1[j].add(n[i])
-            } 
-        }
+fun re(n: Int): Int {
+    if (n > 1) {
+        return n * re(n - 1)
+    } else {
+        return 1
     }
+}
+
+fun main() {
+    print(re(5))
 }
